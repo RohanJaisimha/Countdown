0d4367eefd1fd55837ef591957a3352ea6868082
RohanJaisimha <33845718+RohanJaisimha@users.noreply.github.com>
2018-11-06 18:32:36 -0600

    Update wordsSolver.py

--- wordsSolver.py
+++ wordsSolver.py
@@ -1,3 +1,4 @@
+#goes through the dictionary and checks each word against the letters
 def wordsSolver(letters):
   fin=open("countdownDictionary.txt",'r')
   longest_word=""
@@ -7,6 +8,17 @@ def wordsSolver(letters):
       longest_word=i
   return longest_word
 
+#checks if the all the letters in word are in letters
+#Eg. lettersMatch(["a","b","e","c","i","o","r","s","a"],"rabies")
+#After the first loop, count = {'a':2,'b':1,'c':1,'e':1,'i':1,'o':1,'r':1,'s':1}
+#After the second loop, count = {'a':1,'b':0,'c':1,'e':0,'i':0,'o':1,'r':0,'s':0}
+#As none of the numbers are negative, it returns true
+
+#Eg. lettersMatch(["a","b","e","c","i","o","r","s","a"],"babies")
+#After the first loop, count = {'a':2,'b':1,'c':1,'e':1,'i':1,'o':1,'r':1,'s':1}
+#After the second loop, count = {'a':1,'b':-1,'c':1,'e':0,'i':0,'o':1,'r':1,'s':0}
+#As count['b'] < 0, word has too many b
+#Therefore, it returns False
 def lettersMatch(letters,word):
   count={}
   for i in letters:
